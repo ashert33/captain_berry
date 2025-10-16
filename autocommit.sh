09777692bbced8ddfaf6ed4cb01746082b4e6081
asher tracy <asher.tracy33@gmail.com>
2025-10-16 13:18:01 -0500

    Autocommit from 2025-10-16 13:18:01
    Klipper version: v0.13.0-272-g1be6c0fce
    
    Moonraker version: v0.9.3-113-g596e7d9
    
    Mainsail version: v2.14.0

--- autocommit.sh
+++ autocommit.sh
@@ -85,7 +85,9 @@ fi
 push_config(){
   cd $config_folder
   git pull origin $branch --no-rebase
-  git add .
+  rsync -av --delete ~/printer_data/config/ ./
+  git add -A
+  find . -type d -empty -exec touch {}/.keep \;
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
   git push origin $branch
